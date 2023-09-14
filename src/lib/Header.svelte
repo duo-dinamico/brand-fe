3bbe3cba7f2e14ecfa86a76d85753415a00a8463
Joao Jesus <67010899+joaojesus81@users.noreply.github.com>
2023-09-14 20:28:30 +0100

    added button for login

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,3 +1,10 @@
 <div class="navbar bg-secondary">
 	<h1>Made in Portugal Brands</h1>
+	<a href="/login">
+		<div class="avatar placeholder">
+			<div class="bg-neutral-focus text-neutral-content rounded-full w-16">
+				<span class="text-xl">login</span>
+			</div>
+		</div>
+	</a>
 </div>
